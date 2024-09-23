baffac3d6cd0088019fe67a4dc98c43546666db8
Shivud <shivanand.d@khyath.com>
2024-09-23 15:18:47 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,13 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 // Keep the application running
-        while (true) {
-            try {
-                Thread.sleep(1000); // Sleep for 1 second
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                break;
-            }
-        }
+        
     }
 }
